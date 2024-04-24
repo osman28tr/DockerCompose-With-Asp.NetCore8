cc3b2c89d9c9557ba5042f9014bc7ed57b341204
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-04-24 13:54:22 +0300

    Add project files.

---
+++ MicroService1.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using MicroService1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+        public ProductsController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpClient = new HttpClient();
+            var response =
+                await httpClient.GetAsync(
+                    $"{_configuration.GetSection("Microservices")["baseUrlMicroservice2"]}/api/Products");
+            var content = await response.Content.ReadAsStringAsync();
+            return Ok(content);
+        }
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
---
+++ MicroService1.API/Program.cs
@@ -0,0 +1,27 @@
+using MicroService1.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
